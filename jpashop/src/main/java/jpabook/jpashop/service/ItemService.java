393a4f5ec1eae6a66c8df52a031b2108c251f2f9
suk13574 <suk13574@naver.com>
2022-09-10 11:35:37 +0900

    Spring 공부

---
+++ jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemrepository;
+
+    @Transactional
+    public void saveItem(Item item){
+        itemrepository.save(item);
+    }
+
+    public List<Item> findItem(){
+        return itemrepository.findAll();
+    }
+
+    public Item findOne(Long itemId){
+        return itemrepository.findOne(itemId);
+    }
+
+}
